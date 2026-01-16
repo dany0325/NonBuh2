69f61ae372d542b6fe71867f7426009a0d0a1b58
Kiss Dániel <kissdani0309@gmail.com>
2026-01-16 08:58:45 +0100

    Done()

--- NonBuh/NonBuh/Program.cs
+++ NonBuh/NonBuh/Program.cs
@@ -70,7 +70,13 @@
         public string Season(int month)
         {
             // TODO : Return season based on month value
-            throw new NotImplementedException();
+            if (month > 2 || month < 6)
+            { return "spring"; }
+            else if (month > 5 || month < 9)
+            { return "summer"; }
+            else if (month > 8 || month < 12)
+            { return "fall"; }
+            else { return "winter"; }
         }
 
 
